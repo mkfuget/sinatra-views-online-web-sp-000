de7992709a0967ea3d05afbc6f910ecbb52b8386
Michael Fuget <michaelfuget@gmail.com>
2020-12-23 00:42:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
+    get '/' do
       erb :index
-
-	end
+    end
+ 
+    get "/info" do
+      "Testing the info page"
+    end
 end
